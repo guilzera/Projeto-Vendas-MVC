6ebd97e1e700c1f20cb8cb5837b42d436c9b6bba
Guilherme Nascimento <guiguidefarias@hotmail.com>
2021-01-16 22:32:35 -0300

    Other entities and second migration

--- SalesWebMVC/Data/SalesWebMVCContext.cs
+++ SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -15,5 +15,7 @@ namespace SalesWebMVC.Data
         }
 
         public DbSet<SalesWebMVC.Models.Department> Department { get; set; }
+        public DbSet<SalesWebMVC.Models.Seller> Seller { get; set; }
+        public DbSet<SalesWebMVC.Models.SalesRecord> SalesRecord { get; set; }
     }
 }
